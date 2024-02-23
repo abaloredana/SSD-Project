d5b02af4810b6624b2dd9ea6d77265aa99f8d89e
abaloredana <l.abalorodriguez@usp.ceu.es>
2024-02-23 19:59:11 +0100

    Proyecto con dependencias y archivos vacios

---
+++ diabetes-project/src/diabetes/project/Diabetes.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package diabetes.project;
+
+import static org.apache.tools.ant.dispatch.DispatchUtils.execute;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+
+public class Diabetes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      KieServices ks = KieServices.Factory.get();
+      KieContainer kc = ks.getKieClasspathContainer();
+      execute(ks, kc); // This calls your custom execute method
+  }
+
+    public static void execute(KieServices ks, KieContainer kc) {
+        // From the container, a session is created based on
+        // its definition and configuration in the META-INF/kmodule.xml file
+        KieSession ksession = kc.newKieSession("diabetesSession");
+
+        // Once the session is created, the application can interact with it
+
+        // To set up a ThreadedFileLogger, so that the audit view reflects events whilst debugging,
+        // uncomment the next line
+        // KieRuntimeLogger logger = ks.getLoggers().newThreadedFileLogger( ksession, "./helloworld", 1000 );
+        // The application can insert facts into the session
+        Patient patient = new Patient();
+        patient.setWeight(70);
+        
+        
+        ksession.insert(patient);
+
+        // and fire the rules
+        ksession.fireAllRules();
+
+
+        // and then dispose the session
+        ksession.dispose();
+    }
+    
+}
---
+++ diabetes-project/src/diabetes/project/Patient.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package diabetes.project;
+
+
+import java.util.List;
+/**
+ *
+ * @author loredana
+ */
+public class Patient {
+   
+    private float weight;
+
+    public Patient() {
+        // YOU SHOULD NOT DO
+    }
+
+    public float getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(final float weight) {
+        this.weight = weight;
+    }
+
+    
+    public static Patient doSomething(Patient message) {
+        return message;
+    }
+
+
+}
+
+    
+
